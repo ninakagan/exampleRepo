d6aca025eff0c0c4a0ac7333820e8e68b345fb19
ninakagan <53443754+ninakagan@users.noreply.github.com>
2019-07-29 10:23:56 -0700

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,14 @@
+#---------------------------------------------------
+#              Nina Kagan
+#              7/29/19
+#          sample python file
+#
+#
+#---------------------------------------------------
+
+def main():
+    name = "nina"
+    print("hi my name is", name)
+
+if __name__ == "__main__":
+    main()
